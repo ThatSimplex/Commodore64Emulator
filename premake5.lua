362503bc65e2e90574f393ad4f7464ef8d4c424d
ThatSimplex <jonas.folvik@gmail.com>
2021-06-08 22:31:24 +0200

    Finally staarted my Emulator Project

---
+++ premake5.lua
@@ -0,0 +1,31 @@
+workspace "Commodore64Emulator"
+	architecture "x64"
+
+	configurations {
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+-- Include directories relaative to root folder (solution directory)
+IncludeDir = {}
+
+project "Commodore64Emulator"
+	location "Commodore64Emulator"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++17"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files {
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs {
+		"%{prj.name}/src"
+	}
